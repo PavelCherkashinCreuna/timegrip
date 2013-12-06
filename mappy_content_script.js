0d33cfdaf7d3ef4a8f02dae19472b42dbd995c19
Pavel Cherkashin <pcherk86@gmail.com>
2013-12-06 14:42:49 +0200

    santa time

--- mappy_content_script.js
+++ mappy_content_script.js
@@ -4,5 +4,12 @@ function injectJs(link) {
         scr.src=link;
         (document.head || document.body || document.documentElement).appendChild(scr);
 }
+function injectSanta(link) {
+        var div = document.createElement('div');
+        div.className = 'santa-time';
+        div.style.backgroundImage = 'url("' + link + '")';
+        (document.body || document.documentElement).appendChild(div);
+}
 
+injectSanta(chrome.extension.getURL("santa.png"));
 injectJs(chrome.extension.getURL("script.js"));
